ee245941af7de3fe01c55fdecfef34db772574df
relystate <relystate@gmail.com>
2012-10-16 09:49:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0f918bb009e3aa85fd028996199097823df50b5b2e62b5cac84277c83a3e19c1c4f82abc9789d140f5ad9b00947e96259902979c2ea0bdfb5ab49024fdef8677'
